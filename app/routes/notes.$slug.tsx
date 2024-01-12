1211c24b50bef52277b81702189fcb7d1eef1978
Tom <7273943+tnar@users.noreply.github.com>
2024-01-12 18:31:33 +0900

    give up

--- app/routes/notes.$slug.tsx
+++ app/routes/notes.$slug.tsx
@@ -49,7 +49,7 @@ export default function NotesSlug() {
                 ? `/notes/${prevNote.slug}?tag=${tagSlug}`
                 : `/notes/${prevNote.slug}`
             }
-            className="btn btn-sm md:btn-md gap-2 lg:gap-3 mb-5 max-w-full"
+            className="btn btn-sm md:btn-md gap-2 lg:gap-3 mb-5"
           >
             <svg
               className="h-6 w-6 fill-current md:h-8 md:w-8"
@@ -59,13 +59,8 @@ export default function NotesSlug() {
               viewBox="0 0 24 24"
             >
               <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path>
-            </svg>{" "}
-            <div className="flex flex-col items-start">
-              <span className="text-base-content/50 hidden text-xs font-normal md:block">
-                Prev
-              </span>{" "}
-              <span>{prevNote.title}</span>
-            </div>
+            </svg>
+            <span>Prev</span>
           </Link>
         ) : (
           <div className="btn btn-sm md:btn-md gap-2 lg:gap-3 invisible">
@@ -79,14 +74,9 @@ export default function NotesSlug() {
                 ? `/notes/${nextNote.slug}?tag=${tagSlug}`
                 : `/notes/${nextNote.slug}`
             }
-            className="btn btn-sm md:btn-md gap-2 lg:gap-3 max-w-full"
+            className="btn btn-sm md:btn-md gap-2 lg:gap-3"
           >
-            <div className="flex flex-col items-end">
-              <span className="text-neutral-content/50 hidden text-xs font-normal md:block">
-                Next
-              </span>{" "}
-              <span>{nextNote.title}</span>
-            </div>{" "}
+            <span className="">Next</span>
             <svg
               className="h-6 w-6 fill-current md:h-8 md:w-8"
               xmlns="http://www.w3.org/2000/svg"
